1b4d7e938d257e6b236c12cac71fcdf1f6243777
Jeremiah Knizley <jdknizle@ncsu.edu>
2024-04-04 03:59:41 -0400

    Fixed issue involving redirecting from CSS files to login page.

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/WebSecurityConfig.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/WebSecurityConfig.java
@@ -26,8 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure ( final HttpSecurity http ) throws Exception {
-        http.authorizeRequests().antMatchers( "/home" ).permitAll().anyRequest().authenticated().and().formLogin()
-                .loginPage( "/login" ).permitAll().and().logout().permitAll();
+        http.authorizeRequests().antMatchers( "/css/app.css", "/css/bootstrap.css" ).permitAll().anyRequest()
+                .authenticated().and().formLogin().loginPage( "/login" ).permitAll().and().logout().permitAll();
     }
 
     /**
