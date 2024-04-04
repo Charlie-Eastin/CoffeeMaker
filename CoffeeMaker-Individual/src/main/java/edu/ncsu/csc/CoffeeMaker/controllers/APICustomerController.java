575bcf96b62a388b3f8d0f5c65e8240a1c0c5ac5
Jeremiah Knizley <jdknizle@ncsu.edu>
2024-04-04 02:00:01 -0400

    Added backend support for getting the current user's name and role.

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/APICustomerController.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/controllers/APICustomerController.java
@@ -1,9 +1,17 @@
 package edu.ncsu.csc.CoffeeMaker.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 import edu.ncsu.csc.CoffeeMaker.roles.Customer;
 import edu.ncsu.csc.CoffeeMaker.services.CustomerService;
@@ -29,5 +37,35 @@ public class APICustomerController extends APIController {
         }
 
         return true;
+    }
+
+    @GetMapping ( BASE_PATH + "/username" )
+    public ResponseEntity getUserName () {
+        final Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if ( principal instanceof UserDetails ) {
+            final String username = ( (UserDetails) principal ).getUsername();
+            final Gson gson = new Gson();
+            return new ResponseEntity( gson.toJson( username ), HttpStatus.OK );
+        }
+        else {
+            final String username = principal.toString();
+            final Gson gson = new Gson();
+            return new ResponseEntity( gson.toJson( username ), HttpStatus.OK );
+        }
+    }
+
+    @GetMapping ( BASE_PATH + "/role" )
+    public ResponseEntity getUserRole () {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final Gson gson = new Gson();
+
+        final boolean isStaff = authentication.getAuthorities().stream()
+                .anyMatch( r -> r.getAuthority().equals( "STAFF" ) );
+
+        if ( isStaff ) {
+            return new ResponseEntity( gson.toJson( "STAFF" ), HttpStatus.OK );
+        }
+        return new ResponseEntity( gson.toJson( "CUSTOMER" ), HttpStatus.OK );
     }
 }
